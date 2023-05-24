cd32cca4e5d6876e234ce9c567c81bb8d02a6314
hasheemshike <132909807+hasheemshike@users.noreply.github.com>
2023-05-24 14:10:02 +0800

    Update tile.py

--- Capstone/tile.py
+++ Capstone/tile.py
@@ -9,12 +9,12 @@ class Tile(pygame.sprite.Sprite):
 
 	def update(self,x_shift):
 		self.rect.x += x_shift
-		
+
 class LoseTile(pygame.sprite.Sprite):
 	def __init__(self,pos,size):
 		super().__init__()
 		self.image = pygame.Surface((size,size))
-		self.image.fill('grey')
+		self.image.fill('red')
 		self.rect = self.image.get_rect(topleft = pos)
 
 	def update(self,x_shift):
